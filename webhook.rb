2290eb0918ef7118a9eed572d6b0c18174f201d7
Marcus Baw <marcusbaw@gmail.com>
2018-06-04 11:58:12 +0100

    initial setup

---
+++ webhook.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'httparty'
+require 'json'
+
+# catch the webhook
+post '/webhook' do
+  message = params[:text].gsub(params[:trigger_word], '').strip
+
+  # action, repo = message.split('_').map {|c| c.strip.downcase }
+  # repo_url = "https://api.github.com/repos/#{repo}"
+  #
+  # case action
+  #   when 'issues'
+  #     # resp = HTTParty.get(repo_url)
+  #     # resp = JSON.parse resp.body
+  #     # respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+  # end
+end
+
+def respond_message message
+  content_type :json
+  {:text => message}.to_json
+end
+
+# check_secret_token
+# send SMS messages
+# report success
