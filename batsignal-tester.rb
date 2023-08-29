3a808fc3a54db08b317deff30632abe17b81e88a
Marcus Baw <marcusbaw@gmail.com>
2023-08-29 22:23:26 +0100

    fixes logger

--- batsignal-tester.rb
+++ batsignal-tester.rb
@@ -6,7 +6,7 @@ require 'dotenv'
 require 'logger'
 
 # setup logger
-@logger = Logger.new(STDOUT)
+log = Logger.new(STDOUT)
 
 #load env vars based on deploy status
 if ENV['DEPLOY_STATUS'] == 'live'
@@ -17,7 +17,7 @@ else
   log.debug("Loaded TEST environment")
 end
 
-def send_batsignal
+def send_batsignal(log)
   # set up client
   client = DiscourseApi::Client.new(ENV['DISCOURSE_URL'])
   client.api_key, client.api_username = ENV['DISCOURSE_API_KEY'], ENV['DISCOURSE_USERNAME']
@@ -31,16 +31,16 @@ def send_batsignal
       title: ENV['BATSIGNAL_TEST_TITLE'] + " " + DateTime.now.strftime("%Y.%m.%d"),
       raw: ENV['BATSIGNAL_TEST_TEXT']
     )
-    @logger.info "BATSIGNAL TEST SENT - #{Date.today.strftime('%A')} #{DateTime.now.strftime("%Y.%m.%d")} CATEGORY: #{ENV['DISCOURSE_CATEGORY']}"
+    log.info "BATSIGNAL TEST SENT - #{Date.today.strftime('%A')} #{DateTime.now.strftime("%Y.%m.%d")} CATEGORY: #{ENV['DISCOURSE_CATEGORY']}"
   rescue DiscourseApi::UnprocessableEntity => error
     # `body` is something like `{ errors: ["Name must be at least 3 characters"] }`
     # This outputs "Name must be at least 3 characters"
     # email errors to admin
     puts error.response.body['errors'].first
-    @logger.error "Batsignal sending errored: #{error.response.body['errors'].first}"
+    log.error "Batsignal sending errored: #{error.response.body['errors'].first}"
   end
 end
 
-send_batsignal
+send_batsignal(log)
 
 # email notification to admin
